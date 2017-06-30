a0a0a50baaaa5f8f59c21ec49ad7af69d7170b6b
rajendra7406 <rajendra7406@gmail.com>
2017-06-30 13:44:48 +0530

    Parsing done & stored in pickle file
    Readings are stored in the dictionary form, {[channel 1]:[256 values], [channel 2]:[256 values], ... total 64 channels}
    Dictionary is stored in pickle file

--- parsing.py
+++ parsing.py
@@ -7,18 +7,24 @@ Created on Tue Jun 27 19:41:35 2017
 
 import pickle  
 fo = open('co2a0000364.rd.000')
-count = 4
-while (count>0):
-    print(fo.readline())
-    count=count-1;
 
+#file to be dumped to pickle
+reading = {['name']:[]}
+
+for count in range(1,5):
+    print(fo.readline())
+
+reading['name'] = 'raju'
+reading['name'].append('raju2')
 m=[]
 
 for channels in range(1,65):
     for readingCount in range(1,258):
         readingValue = fo.readline().split()
         m.append(readingValue)
-            
+        reading['']
+
+fo.close()            
         
     
     
