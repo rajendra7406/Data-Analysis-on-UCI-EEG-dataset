e46bb6c11eebecfee6de4402da1c56e7209f539d
rajendra7406 <rajendra7406@gmail.com>
2017-07-02 11:19:07 +0530

    Parsing done & stored in pickle file

--- parsing.py
+++ parsing.py
@@ -5,25 +5,36 @@ Created on Tue Jun 27 19:41:35 2017
 @author: Chaitra
 """
 
-import pickle  
 fo = open('co2a0000364.rd.000')
 
-#file to be dumped to pickle
-reading = {['name']:[]}
+from collections import defaultdict
+#defining dictionary
 
+readingsDict = defaultdict(list)
+
+my_list = [(1234, 100.23),(1234,123), (345, 10.45), (1234, 75.00),
+           (345, 222.66), (678, 300.25), (1234, 35.67)]
+
+
+
+#skipping header lines
 for count in range(1,5):
     print(fo.readline())
 
-reading['name'] = 'raju'
-reading['name'].append('raju2')
-m=[]
-
+#looping from 6th line
 for channels in range(1,65):
     for readingCount in range(1,258):
-        readingValue = fo.readline().split()
-        m.append(readingValue)
-        reading['']
+        readingValue = fo.readline().split() 
+        if readingCount!=1:
+            readingsDict[readingValue[1]].append(readingValue[3]) 
+        
 
+import pickle  
+#dump readingsDict to pickle
+with open('readings.pickle', 'wb') as f: 
+    pickle.dump(readingsDict, f)
+
+#closing the file
 fo.close()            
         
     
